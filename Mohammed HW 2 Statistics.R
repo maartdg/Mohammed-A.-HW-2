5dce2e4c4999f617a196584002ef685876aedd57
maartdg <143771467+maartdg@users.noreply.github.com>
2023-09-14 22:16:18 -0400

    Add files via upload

---
+++ Mohammed HW 2 Statistics.R
@@ -0,0 +1,53 @@
+#Mohammed A.Al Muhaymin
+#H.W 2
+#Statistics and Introduction to Econometrics
+
+#Group Member: Mohammed A. Al Muhaymin, Zakaria Sule
+
+#PP1:
+  
+  #Our first roll was a 5, this means that the dice is fair because it did not roll a 6.
+  #The probability that it will judged to be unfair is 5/6 or 83.3 percent if it is fair dice.
+  #If it was unfair, the probability will be that it depends based on how one makes it unfair.
+  
+  PP2:
+  
+  #Our trial -> pp2 <- c(6,2,5,4,2,2,2,5,5,4,4,2,4,3,1,6,6,3,3,1)
+#We tried stimulating and we did it manually as well, in both cases we rolled 6 three times
+#When we stimulated the roll of the dice 20 times, we got 6 three times
+#Our decision rule is that it is fair if 6 is rolled three times or less 
+#and unfair if 6 is rolled four times or more.
+#If dice is rolled 20 times, likely number 6 results is  (1/6) * 20 =  3.33
+#If the dice is 20 times and the dice is fair, the probability that is unfair is 43.3 percent 
+#becaue P(X >= 4) = 0.433
+
+
+  
+
+#PP3
+
+
+#We tried the stimulation many times and did not got any thing more than 24
+#Our decision rule is that, if the dice is rolled 25 times or higher it is unfair, if the dice is rolled 24 times or less it is fair.
+#When we stimulated the roll of the 100 ties, we got 6 12 times
+#Since 12 < 24, we can say the dice is fair
+#If the dice is rolled 100 times, likely number of 6 result is (1/6) * 100 = 16.67
+#If the dice is fair, The probability that the dice would be unfair is 2.17 percent 
+#because P(x >= 25) = 0.0217
+
+#EP1:
+#From our class trial, we rolled manually and not manually got six 20 times combining the manual and non manual
+#Together the trial comes to 160 trials
+# Decision Rule: 6 is rolled 32 or more it is unfair 
+#Since six is rolled 20 times it is fair
+#I would add more trials and see how the probability changes
+  
+  
+
+
+
+
+
+
+
+
